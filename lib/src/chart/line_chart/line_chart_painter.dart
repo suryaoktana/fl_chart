96fa011a80ae368fcf83f69310ea5b3b628c9672
suryaoktana <50902263+suryaoktana@users.noreply.github.com>
2022-10-21 16:13:53 +0700

    Add files via upload
    bug fix

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -8,7 +8,6 @@ import 'package:fl_chart/src/chart/base/base_chart/base_chart_painter.dart';
 import 'package:fl_chart/src/extensions/paint_extension.dart';
 import 'package:fl_chart/src/extensions/path_extension.dart';
 import 'package:fl_chart/src/utils/canvas_wrapper.dart';
-import 'package:fl_chart/src/chart/custom_paint/icon_buy_painter.dart';
 import 'package:flutter/material.dart';
 import 'package:path_drawing/path_drawing.dart';
 import '../../extensions/text_align_extension.dart';
